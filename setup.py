e89e49f136b1968a57946a90d206a502573e88bc
Dominik Roessler <dominik@freshx.de>
2013-01-15 12:49:55 +0100

    added license information to setup file

--- setup.py
+++ setup.py
@@ -9,6 +9,10 @@ setup(name='wallaby-backend-couchdb',
       url='https://github.com/FreshXOpenSource/wallaby-backend-couchdb',
       author='FreshX GbR',
       author_email='wallaby@freshx.de',
+      license='BSD',
+      description='Wallaby backend for CouchDB.',
+      long_description=open('README.md').read(),
+      package_data={'': ['LICENSE', 'AUTHORS', 'README.md']},
       packages=find_packages('.'),
       install_requires=['wallaby-backend-http']
   )
